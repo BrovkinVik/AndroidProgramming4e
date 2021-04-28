c12269270e32f3c046a2af209788a468c07dfc01
BrovkinVik <vikbrovkin@gmail.com>
2021-04-28 16:02:51 +0500

    Added a second activity (CheatActivity) to the app.

--- app/src/main/java/ru/vikbrovkin/android/geoquiz/MainActivity.kt
+++ app/src/main/java/ru/vikbrovkin/android/geoquiz/MainActivity.kt
@@ -1,5 +1,6 @@
 package ru.vikbrovkin.android.geoquiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
@@ -21,6 +22,7 @@ class MainActivity : AppCompatActivity() {
     }
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
+    private lateinit var cheatButton: Button
     private lateinit var prevButton: Button
     private lateinit var nextButton: Button
     private lateinit var questionTextView: TextView
@@ -36,6 +38,7 @@ class MainActivity : AppCompatActivity() {
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
+        cheatButton = findViewById(R.id.cheat_button)
         prevButton = findViewById(R.id.prev_button)
         nextButton = findViewById(R.id.next_button)
         questionTextView = findViewById(R.id.question_text_view)
@@ -52,6 +55,12 @@ class MainActivity : AppCompatActivity() {
             buttonsEnabled(false)
             quizViewModel.questionIsEnabled(false)
             quizViewModel.countDisabled += 1
+        }
+
+        cheatButton.setOnClickListener {
+            // Begin CheatActivity
+            val intent = Intent(this, CheatActivity::class.java)
+            startActivity(intent)
         }
 
         prevButton.setOnClickListener {
